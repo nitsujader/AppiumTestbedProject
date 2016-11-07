6492c6151af6bbd79f2260fe2ec472c0f66e26db
justinreda <justinreda@gmail.com>
2016-11-07 00:47:00 -0500

    jenkins test commit 3

--- app/src/main/java/com/justinreda/appiumtestbedproject/MainActivity.java
+++ app/src/main/java/com/justinreda/appiumtestbedproject/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //jenkins test 3
+        //jenkins test 4
     }
 }
